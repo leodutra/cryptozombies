b5ff501f4cd1b50533eb453dc0d68f7ace15519a
Leo Dutra <leodutra.br@gmail.com>
2024-11-04 03:34:15 -0300

    Enhance ZombieFactory contract with ownership management and cooldown feature

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
-contract ZombieFactory {
+import {Ownable} from "./Ownable.sol";
+
+contract ZombieFactory is Ownable {
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -18,14 +23,19 @@ contract ZombieFactory {
     mapping(address => uint) ownerZombieCount;
 
     function createRandomZombie(string memory _name) public {
-        require(ownerZombieCount[msg.sender] == 0, "User already has a zombie.");
+        require(
+            ownerZombieCount[msg.sender] == 0,
+            "User already has a zombie."
+        );
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
     function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.length;
-        zombies.push(Zombie(_name, _dna));
+        zombies.push(
+            Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime))
+        );
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
